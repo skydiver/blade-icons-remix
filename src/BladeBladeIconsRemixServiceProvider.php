6a9d5a7abe5a431a993c773911935b13d465e4ab
Martín M <skydiver@users.noreply.github.com>
2020-07-25 21:45:24 -0300

    service provider created

---
+++ src/BladeBladeIconsRemixServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Skydiver\BladeBladeIconsRemix;
+
+use BladeUI\Icons\Factory;
+use Illuminate\Support\ServiceProvider;
+
+final class BladeBladeIconsRemixServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        $this->callAfterResolving(Factory::class, function (Factory $factory) {
+            $factory->add('remix', [
+                'path' => __DIR__ . '/../resources/svg',
+                'prefix' => 'remix',
+            ]);
+        });
+    }
+
+    public function boot(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../resources/svg' => public_path('vendor/blade-icons-remix'),
+            ], 'blade-icons-remix');
+        }
+    }
+}
